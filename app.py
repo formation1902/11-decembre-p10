1506491ed4bc8ba8069926891ae7c8aed5bcf955
msaP9 <formation1902@gmail.com>
2022-12-11 20:12:25 +0100

    new 11 decembre repos for p10

---
+++ app.py
@@ -0,0 +1,162 @@
+from botbuilder.core    import BotFrameworkAdapterSettings
+from botbuilder.core    import ConversationState,    MemoryStorage,    UserState
+from botbuilder.core    import TelemetryLoggerMiddleware
+#
+from botbuilder.schema  import Activity
+#
+from botbuilder.applicationinsights                     import ApplicationInsightsTelemetryClient
+from botbuilder.integration.applicationinsights.aiohttp import AiohttpTelemetryProcessor
+#
+from dialogs  import MainDialog, ReservationDialog
+from bots     import DialogAndWelcomeBot,DialogBot
+from adapter_with_error_handler            import AdapterWithErrorHandler
+from Reserver_un_billet_d_avion_Recognizer import Reserver_un_billet_d_avion_Recognizer
+from config import Bot_luis_app_and_insights_configuration
+#
+from http import HTTPStatus
+from aiohttp        import web as aiohttp_web
+from aiohttp.web    import Request, Response, json_response
+#
+
+#######################################################
+# Creation de l'adaptateur :
+#   - lecture de la configuration
+#   - creation des espaces memoires
+#   - configuration et activation de la telemetrie
+#######################################################
+# ----> Lecture de la configuration : 
+CONFIG      = Bot_luis_app_and_insights_configuration()
+Adapter_SETTINGS    = BotFrameworkAdapterSettings(CONFIG.APP_ID)#, CONFIG.APP_PASSWORD)
+
+# ---> Espace temporaire
+MEMORY              = MemoryStorage()
+USER_STATE          = UserState(MEMORY)
+CONVERSATION_STATE  = ConversationState(MEMORY)
+
+# ---> Telemetrie appinsights
+#
+# Create telemetry client. Note the small 'client_queue_size'.  This is for demonstration purposes.  
+# Larger queue sizes result in fewer calls to ApplicationInsights, improving bot performance at the expense of less frequent updates.
+INSTRUMENTATION_KEY = CONFIG.APPINSIGHTS_INSTRUMENTATION_KEY
+TELEMETRY_CLIENT = ApplicationInsightsTelemetryClient(
+    INSTRUMENTATION_KEY, 
+    telemetry_processor=AiohttpTelemetryProcessor(), 
+    client_queue_size=10
+)
+
+# ---> Logging :  Code for enabling activity and personal information logging.
+TELEMETRY_LOGGER_MIDDLEWARE = TelemetryLoggerMiddleware(telemetry_client=TELEMETRY_CLIENT, log_personal_information=True)
+
+#######################################################
+# - creation de l'adapter
+# Creation des dialogs et du bot
+# 
+#######################################################
+
+# ---> Definition de l'adaptateur : 
+ADAPTER = AdapterWithErrorHandler(Adapter_SETTINGS, CONVERSATION_STATE)
+ADAPTER.use(TELEMETRY_LOGGER_MIDDLEWARE)
+
+print("L'adaptateur : ",dir(ADAPTER),"\n\n")
+RECOGNIZER          = Reserver_un_billet_d_avion_Recognizer(CONFIG)
+Reservation_DIALOG  = ReservationDialog()
+DIALOG          = MainDialog(RECOGNIZER, Reservation_DIALOG, telemetry_client=TELEMETRY_CLIENT)
+BOT             = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG, TELEMETRY_CLIENT)
+
+
+#
+async def fx_handle_new_user_message_to_bot_api(req: Request) -> Response:
+    # 
+    # On n accepte que de JSON
+    #
+    
+    print("----> [App - fx_handle_new_user_message_to_bot_api : Handling new user message ]")
+    print("\t ----> [App - fx_handle_new_user_message_to_bot_api : 1. receiving user text ]")
+    if "application/json" in req.headers["Content-Type"]:
+        body = await req.json()
+        print("\t----> [App - fx_handle_new_user_message_to_bot_api ] received request :  ",body)
+    else:
+        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+
+    #
+    # Ce qui est recu est transformée en un nouveau objet Activity incluant  the authentication header 
+    #
+    print("\n\n############ body : \n",body,"\n\n")
+    print("\t ----> [App - fx_handle_new_user_message_to_bot_api : 2. creating the Activity object ]")
+    activity = Activity().deserialize(body)
+    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
+
+    #
+    # Notre object Activity est renvoyée à la methode process_activity de l'adaptateur
+    #
+    print("\t ----> [App - fx_handle_new_user_message_to_bot_api : 3. Calling the adapter with the activtity object ]")
+    print("\n\n############ BOT.on_turn : \n",BOT.on_turn,"\n\n")
+    print("App - fx_handle_new_user_message_to_bot_api : 3 --- START")
+    response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+    #
+    # - L'adaptateur cree le context 
+    # - L'adaptateur envoie le context crée au pipeline middleware
+    # - L'adaptateur continue par envoyer le context au "turn handler" du bot : BOT.on_turn
+    # - L'adaptateur termine par formatter et retourner the response
+    # - Durant ce temps, l'adaptateur reste le dernier recours des exceptions generees non prises en charge
+    #
+    
+    if response:
+        print("\t ----> [App - fx_handle_new_user_message_to_bot_api : received response from the adapter ]")
+        print("\t\t - responde.body : ",response.body)
+        print("\t\t - responde.status : ",response.status)
+        return json_response(data=response.body, status=response.status)
+    else:
+        print("\t ----> [App - fx_handle_new_user_message_to_bot_api : OK with no received response from the adapter]")
+    print("App - fx_handle_new_user_message_to_bot_api : 3 --- END\n\n")
+    return Response(status=HTTPStatus.OK)
+
+
+
+#
+# Finally : 
+#
+
+def fx_init_app(argv=None):
+    #
+    # Declaration de l'application 
+    #
+    print("INFO: [App.py -  fx_init_app ]  fx_init_app called : argv==",argv)
+    #
+    from botbuilder.integration.applicationinsights.aiohttp import bot_telemetry_middleware
+    from botbuilder.core.integration                        import aiohttp_error_middleware
+    #
+    APP = aiohttp_web.Application(
+        middlewares = [            
+                bot_telemetry_middleware,             
+                aiohttp_error_middleware        
+        ]
+    )
+    print("\tINFO: [App.py -  msa fx_init_app ] APP created")  
+    #
+    # Definition des EndPoints
+    #
+    APP.router.add_post("/p10/api/messages", fx_handle_new_user_message_to_bot_api) 
+    
+    print("\tINFO: [App.py -  fx_init_app ] APP endpoint configured")
+    print("\tINFO: [app.py -  fx_init_app ] Nombre d'objets DialogAndWelcomeBot ==",DialogAndWelcomeBot.nb)
+    print("\tINFO: [app.py -  fx_init_app ] Nombre d'objets DialogBot           ==",DialogBot.nb)
+    return APP
+
+APP = fx_init_app()
+
+
+if __name__ == "__main__":
+    print("INFO: [App.py - main ] start running the bot APP")
+    try:
+        aiohttp_web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
+
+
+#
+# Startup classique
+#
+# gunicorn --bind 0.0.0.0 --worker-class aiohttp.worker.GunicornWebWorker --timeout 600 -P 3978 app:APP
+
+# python -m aiohttp.web -H 0.0.0.0 -P 8000 app:fx_init_app
---
+++ config.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+
+
+class Bot_luis_app_and_insights_configuration:
+    #
+    # Elements pour la configuration de l'application chatbot : 
+    #   - ___________________________PORT: port du service pour le bot emulator
+    #   - _________________________APP_ID: 
+    #   - ___________________APP_PASSWORD: 
+    #   - ____________________LUIS_APP_ID: 
+    #   - ___________________LUIS_API_KEY: 
+    #   - _____________LUIS_API_HOST_NAME: 
+    #   - APPINSIGHTS_INSTRUMENTATION_KEY: app-insight
+    #
+    
+    PORT = 3978
+    
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    
+    LUIS_APP_ID = os.environ.get("LuisAppId","fa851bde-e539-4c83-b9ef-2f36ca8e1b44")
+    
+    LUIS_API_KEY = os.environ.get("LuisAPIKey","898a47800608435ea33ccde7f880abc5")
+    
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName","p10-luis-authoring.cognitiveservices.azure.com/")
+    # 
+    # LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName","westeurope.api.cognitive.microsoft.com")
+
+    # LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName","p10-luis.cognitiveservices.azure.com")
+    
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        "AppInsightsInstrumentationKey", "a8995314-2a9d-4720-b3ad-cba0cbd2258c"
+    )
+    
+    
+# https://p10-luis.cognitiveservices.azure.com/luis/prediction/v3.0/apps/cf2d6334-8bf9-459a-8a52-573d90fa218c/slots/staging/predict?verbose=true&show-all-intents=true&log=true&subscription-key=a328ef4940ad427db28faf097810819c&query=YOUR_QUERY_HERE    
+# ywr0zn4eao2cjy8jd8dsc8f8qbzm04exxs204cfq
+# secret    : cCh8Q~DYaC65HHHrpFs9K0KxzFldic.6W7eF-cp3
+# secret-id : 652e1a58-6963-4a32-a4b4-9e1c2a1db355
+
+# https://msa-p10-appservicename-svc-name.azurewebsites.net:8000/p10/api/messages
